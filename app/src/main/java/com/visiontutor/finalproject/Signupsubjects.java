eab000bde38dcd92a2e54b09b32ca614d17e8ea8
Harshit Bhootra <harshitbhootra@live.com>
2018-03-05 20:52:05 +0530

    added signup activities for tutor

---
+++ app/src/main/java/com/visiontutor/finalproject/Signupsubjects.java
@@ -0,0 +1,13 @@
+package com.visiontutor.finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Signupsubjects extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signupsubjects);
+    }
+}
