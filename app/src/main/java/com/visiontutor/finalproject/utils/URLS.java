c74fd558592da4bde9d267c5eee5954e9edeedaf
Kartik Patodi <patodi.kartik@gmail.com>
2018-03-21 21:57:30 +0530

    added some tutor urls

--- app/src/main/java/com/visiontutor/finalproject/utils/URLS.java
+++ app/src/main/java/com/visiontutor/finalproject/utils/URLS.java
@@ -7,6 +7,10 @@ public interface URLS {
     String STUDENT_FORGET = API+"studentforget";
     String STUDENT_REGISTER = API+"studentregister";
     String STUDENT_PROFILE = API+"student/";
+    String TUTOR_LOGIN = API+"tutorlogin";
+    String TUTOR_FORGET = API+"tutorforget";
+    String TUTOR_REGISTER = API+"tutorregister";
+    String TUTOR_PROFILE = API+"tutor/";
     String CITIES = API+"cities";
     String CLASSES = API+"classes";
     String SUBJECTS = API+"subjects/";
