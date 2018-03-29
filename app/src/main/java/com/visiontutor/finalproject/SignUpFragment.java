1b39dc457a47d127c58ffd653a7e193630af9293
Kartik Patodi <patodi.kartik@gmail.com>
2018-03-29 14:33:30 +0530

    changes

--- app/src/main/java/com/visiontutor/finalproject/SignUpFragment.java
+++ app/src/main/java/com/visiontutor/finalproject/SignUpFragment.java
@@ -1,6 +1,7 @@
 package com.visiontutor.finalproject;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,111 +19,108 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SignUpFragment extends Fragment implements OnClickListener {
-	private static View view;
-	private static EditText fullName, emailId, mobileNumber, location,
-			password, confirmPassword;
-	private static TextView login;
-	private static Button signUpButton;
-	private static CheckBox terms_conditions;
+    private View view;
+    private EditText fullName, emailId, mobileNumber, location,
+            password, confirmPassword;
+    private TextView login;
+    private Button signUpButton;
+    private CheckBox terms_conditions;
 
-	public SignUpFragment() {
+    public SignUpFragment() {
 
-	}
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		view = inflater.inflate(R.layout.signup_layout, container, false);
-		initViews();
-		setListeners();
-		return view;
-	}
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.signup_layout, container, false);
+        initViews();
+        setListeners();
+        return view;
+    }
 
-	// Initialize all views
-	private void initViews() {
-		fullName = view.findViewById(R.id.fullName);
-		emailId = view.findViewById(R.id.userEmailId);
-		mobileNumber = view.findViewById(R.id.mobileNumber);
-		location = view.findViewById(R.id.location);
-		password = view.findViewById(R.id.password);
-		confirmPassword = view.findViewById(R.id.confirmPassword);
-		signUpButton = view.findViewById(R.id.signUpBtn);
-		login = view.findViewById(R.id.already_user);
-		terms_conditions = view.findViewById(R.id.terms_conditions);
+    // Initialize all views
+    private void initViews() {
+        fullName = view.findViewById(R.id.fullName);
+        emailId = view.findViewById(R.id.userEmailId);
+        mobileNumber = view.findViewById(R.id.mobileNumber);
+        location = view.findViewById(R.id.location);
+        password = view.findViewById(R.id.password);
+        confirmPassword = view.findViewById(R.id.confirmPassword);
+        signUpButton = view.findViewById(R.id.signUpBtn);
+        login = view.findViewById(R.id.already_user);
+        terms_conditions = view.findViewById(R.id.terms_conditions);
+    }
 
+    // Set Listeners
+    private void setListeners() {
+        signUpButton.setOnClickListener(this);
+        login.setOnClickListener(this);
+    }
 
-	}
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.signUpBtn:
 
-	// Set Listeners
-	private void setListeners() {
-		signUpButton.setOnClickListener(this);
-		login.setOnClickListener(this);
-	}
+                // Call checkValidation method
+                checkValidation();
+                break;
 
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.signUpBtn:
+            case R.id.already_user:
 
-			// Call checkValidation method
-			checkValidation();
-			break;
+                // Replace login fragment
+                new MainActivity().replaceLoginFragment();
+                break;
+        }
 
-		case R.id.already_user:
+    }
 
-			// Replace login fragment
-			new MainActivity().replaceLoginFragment();
-			break;
-		}
+    // Check Validation Method
+    private void checkValidation() {
 
-	}
+        // Get all edittext texts
+        String getFullName = fullName.getText().toString();
+        String getEmailId = emailId.getText().toString();
+        String getMobileNumber = mobileNumber.getText().toString();
+        String getLocation = location.getText().toString();
+        String getPassword = password.getText().toString();
+        String getConfirmPassword = confirmPassword.getText().toString();
 
-	// Check Validation Method
-	private void checkValidation() {
+        // Pattern match for email id
+        Pattern p = Pattern.compile(Utils.regEx);
+        Matcher m = p.matcher(getEmailId);
 
-		// Get all edittext texts
-		String getFullName = fullName.getText().toString();
-		String getEmailId = emailId.getText().toString();
-		String getMobileNumber = mobileNumber.getText().toString();
-		String getLocation = location.getText().toString();
-		String getPassword = password.getText().toString();
-		String getConfirmPassword = confirmPassword.getText().toString();
+        // Check if all strings are null or not
+        if (getFullName.isEmpty()
+                || getEmailId.isEmpty()
+                || getMobileNumber.isEmpty()
+                || getLocation.isEmpty()
+                || getPassword.isEmpty()
+                || getConfirmPassword.isEmpty())
 
-		// Pattern match for email id
-		Pattern p = Pattern.compile(Utils.regEx);
-		Matcher m = p.matcher(getEmailId);
+            new CustomToast().Show_Toast(getActivity(), view,
+                    "All fields are required.");
 
-		// Check if all strings are null or not
-		if (getFullName.equals("") || getFullName.length() == 0
-				|| getEmailId.equals("") || getEmailId.length() == 0
-				|| getMobileNumber.equals("") || getMobileNumber.length() == 0
-				|| getLocation.equals("") || getLocation.length() == 0
-				|| getPassword.equals("") || getPassword.length() == 0
-				|| getConfirmPassword.equals("")
-				|| getConfirmPassword.length() == 0)
+            // Check if email id valid or not
+        else if (!m.find())
+            new CustomToast().Show_Toast(getActivity(), view,
+                    "Your Email Id is Invalid.");
 
-			new CustomToast().Show_Toast(getActivity(), view,
-					"All fields are required.");
+            // Check if both password should be equal
+        else if (!getConfirmPassword.equals(getPassword))
+            new CustomToast().Show_Toast(getActivity(), view,
+                    "Both password doesn't match.");
 
-		// Check if email id valid or not
-		else if (!m.find())
-			new CustomToast().Show_Toast(getActivity(), view,
-					"Your Email Id is Invalid.");
+            // Make sure user should check Terms and Conditions checkbox
+        else if (!terms_conditions.isChecked())
+            new CustomToast().Show_Toast(getActivity(), view,
+                    "Please select Terms and Conditions.");
 
-		// Check if both password should be equal
-		else if (!getConfirmPassword.equals(getPassword))
-			new CustomToast().Show_Toast(getActivity(), view,
-					"Both password doesn't match.");
+            // Else do signup or do your stuff
+        else
+            Toast.makeText(getActivity(), "Do SignUp.", Toast.LENGTH_SHORT)
+                    .show();
 
-		// Make sure user should check Terms and Conditions checkbox
-		else if (!terms_conditions.isChecked())
-			new CustomToast().Show_Toast(getActivity(), view,
-					"Please select Terms and Conditions.");
-
-		// Else do signup or do your stuff
-		else
-			Toast.makeText(getActivity(), "Do SignUp.", Toast.LENGTH_SHORT)
-					.show();
-
-	}
+    }
 }
