b3bc911d8b8f77b04cc4f281e26450747c106627
Kartik Patodi <patodi.kartik@gmail.com>
2018-05-26 03:37:56 +0530

    migrated MainActivity to Kotlin

---
+++ app/src/main/java/com/visiontutor/app/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.visiontutor.app
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.transaction
+import com.visiontutor.app.utils.Utils
+
+class MainActivity : AppCompatActivity() {
+
+    val fragmentManager = supportFragmentManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // If savedinstnacestate is null then replace login fragment
+        if (savedInstanceState == null) {
+            fragmentManager.transaction(now = false, allowStateLoss = false) {
+                replace(R.id.frameContainer, LoginFragment(), Utils.Login_Fragment)
+            }
+        }
+
+        // On close icon click finish activity
+        findViewById<View>(R.id.close_activity)
+                .setOnClickListener {
+                    finish()
+                }
+    }
+
+    // Replace Login Fragment with animation
+    fun replaceLoginFragment() {
+        fragmentManager.transaction(now = false, allowStateLoss = false) {
+            setCustomAnimations(R.anim.left_enter, R.anim.right_enter)
+            replace(R.id.frameContainer, LoginFragment(), Utils.Login_Fragment)
+        }
+    }
+
+    override fun onBackPressed() {
+
+        // Find the tag of signup and forgot password fragment
+        val SignUp_Fragment = fragmentManager
+                .findFragmentByTag(Utils.SignUp_Fragment)
+        val ForgotPassword_Fragment = fragmentManager
+                .findFragmentByTag(Utils.ForgotPassword_Fragment)
+
+        // Check if both are null or not
+        // If both are not null then replace login fragment else do backpressed
+        // task
+
+        when {
+            SignUp_Fragment != null -> replaceLoginFragment()
+            ForgotPassword_Fragment != null -> replaceLoginFragment()
+            else -> super.onBackPressed()
+        }
+    }
+}
