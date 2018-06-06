0009d8df89216547e320100fef91f9342850de11
Kartik Patodi <patodi.kartik@gmail.com>
2018-06-07 02:55:43 +0530

    ignore @ 7.6.2018

--- app/src/main/java/com/visiontutor/app/MainApplication.kt
+++ app/src/main/java/com/visiontutor/app/MainApplication.kt
@@ -6,13 +6,7 @@ import com.androidnetworking.AndroidNetworking
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-//        Crouton.cancelAllCroutons()
         AndroidNetworking.initialize(this)
-
     }
 
-}
-
-fun main(args: Array<String>) {
-    print("Test")
 }
